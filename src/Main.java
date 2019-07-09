aa99daff677d69a2dc025e2b771036f6844a2afd
Alexandros Konstantinidis <alkon134@gmail.com>
2019-07-09 19:52:57 +0300

    first commit - create Spitter

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	Spitter spitter = new Spitter("Alex","kon","user4","pass3","alex@kon.com");
+	
+	if(spitter.isUsernameUnique()) {
+	
+	spitter.createSpitter();
+	}
+		
+	}
+
+
+}
---
+++ src/Spittle.java
@@ -0,0 +1,47 @@
+
+public class Spittle {
+
+int id;
+	
+	Spitter spitter;
+
+	String post;	
+
+	public Spittle(Spitter spitter, String post) {
+		super();
+		this.spitter = spitter;
+		this.post = post;
+	}
+
+	public Spittle(int id, Spitter spitter, String post) {
+		super();
+		this.id = id;
+		this.spitter = spitter;
+		this.post = post;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Spitter getSpitter() {
+		return spitter;
+	}
+
+	public void setSpitter(Spitter spitter) {
+		this.spitter = spitter;
+	}
+
+	public String getPost() {
+		return post;
+	}
+
+	public void setPost(String post) {
+		this.post = post;
+	}	
+	
+}
